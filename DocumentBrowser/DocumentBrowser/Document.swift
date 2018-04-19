8da0ff1bb2dd3f24e749e28d8d597dc81e1eb642
djagausa <djagausa@gmail.com>
2018-04-19 08:22:09 -0400

    Adding some comments.

--- DocumentBrowser/DocumentBrowser/Document.swift
+++ DocumentBrowser/DocumentBrowser/Document.swift
@@ -13,6 +13,7 @@ class Document: UIDocument {
     var userText: String? = ""
     
     override func contents(forType typeName: String) throws -> Any {
+        //get content
         if let content = userText {
             let length = content.lengthOfBytes(using: String.Encoding.utf8)
             return NSData(bytes: content, length: length)
@@ -22,6 +23,7 @@ class Document: UIDocument {
     }
     
     override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // get userText
         if let userContent = contents as? Data {
             userText = NSString(bytes: (contents as AnyObject).bytes, length: userContent.count, encoding: String.Encoding.utf8.rawValue) as String?
         }
